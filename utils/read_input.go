6549809b2d6f95780c153e4bf9bfe9c09d4baf95
Mitch Van Rechem <mitch.vanrechem@inthepocket.com>
2022-12-19 00:54:25 +0100

    ✨ Expand file reader for reading to bytes

--- utils/read_input.go
+++ utils/read_input.go
@@ -1,22 +1,55 @@
 package utils
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"os"
 	"strings"
 )
 
-func ReadInput(filePath string) []string {
+func ReadInputAsStrings(filePath string) []string {
 	data, err := os.ReadFile(filePath)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	return strings.Split(string(data), "\n")
+	inputLines := strings.Split(string(data), "\n")
+	lines := []string{}
+
+	for _, inputLine := range inputLines {
+		if inputLine != "" {
+			lines = append(lines, inputLine)
+		}
+	}
+
+	return lines
+}
+
+func ReadInputAsBytes(filePath string) [][]byte {
+	f, err := os.Open(filePath)
+	if err != nil {
+		log.Fatal("unable to read file: ", err)
+	}
+
+	defer f.Close()
+	scanner := bufio.NewScanner(f)
+
+	byteLines := [][]byte{}
+
+	for scanner.Scan() {
+		line := scanner.Bytes()
+		byteLines = append(byteLines, line)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return byteLines
 }
 
 func PrintInput(filePath string) {
-	input := ReadInput(filePath)
+	input := ReadInputAsStrings(filePath)
 	fmt.Print(input)
 }
