3e02adeae73be60e1878b94a1e741aaa9b21fcd5
Mitch Van Rechem <mitch.vanrechem@inthepocket.com>
2022-12-20 13:50:09 +0100

    ✨ Add day 4 part 2 solution

--- day_4/solution.go
+++ day_4/solution.go
@@ -15,17 +15,15 @@ func main() {
 
 	assignemntPairs := parseInput(inputLines)
 	solution1 := part1(assignemntPairs)
+	solution2 := part2(assignemntPairs)
 
 	utils.PrintSolution(&[]string{
 		fmt.Sprintf("part1: %d", solution1),
+		fmt.Sprintf("part1: %d", solution2),
 	})
 }
 
 func part1(assignemntPairs []AssignmentPair) int {
-	// example contained ranges
-	// 2-4, 1-4
-	// 3-7, 5-6
-
 	containedPairs := 0
 
 	for _, assignmentPair := range assignemntPairs {
@@ -40,6 +38,21 @@ func part1(assignemntPairs []AssignmentPair) int {
 	return containedPairs
 }
 
+func part2(assignemntPairs []AssignmentPair) int {
+	overlappingPairs := 0
+
+	for _, assignmentPair := range assignemntPairs {
+		sectionRange1 := strings.Split(string(assignmentPair[0]), "-")
+		sectionRange2 := strings.Split(string(assignmentPair[1]), "-")
+
+		if areRangesOverlapping(sectionRange1, sectionRange2) {
+			overlappingPairs++
+		}
+	}
+
+	return overlappingPairs
+}
+
 func parseInput(lines []string) []AssignmentPair {
 	assignemntPairs := []AssignmentPair{}
 
@@ -48,7 +61,7 @@ func parseInput(lines []string) []AssignmentPair {
 	for _, line := range lines {
 		// assignments -> ["19-73","18-73"]
 		assignments := strings.Split(line, ",")
-		// assignmentPair -> ["19-73","18-73"]
+		// AssignmentPair type -> ["19-73","18-73"]
 		// assignmentPais -> [ ["19-73","18-73"] , ["1-7","8-7"] ]
 		assignemntPairs = append(assignemntPairs, AssignmentPair{assignments[0], assignments[1]})
 	}
@@ -57,6 +70,10 @@ func parseInput(lines []string) []AssignmentPair {
 }
 
 func isOneRangeContained(sectionRange1 []string, sectionRange2 []string) bool {
+	// example contained ranges
+	// 2-4, 1-4
+	// 3-7, 5-6
+
 	range1Lower, _ := strconv.Atoi(sectionRange1[0])
 	range1Upper, _ := strconv.Atoi(sectionRange1[1])
 	range2Lower, _ := strconv.Atoi(sectionRange2[0])
@@ -65,6 +82,27 @@ func isOneRangeContained(sectionRange1 []string, sectionRange2 []string) bool {
 	if range1Lower <= range2Lower && range1Upper >= range2Upper {
 		return true
 	} else if range2Lower <= range1Lower && range2Upper >= range1Upper {
+		return true
+	}
+
+	return false
+}
+
+func areRangesOverlapping(sectionRange1 []string, sectionRange2 []string) bool {
+	// example overlapping ranges, contained ranges still count
+	// 2-4, 3-8
+	// 3-7, 1-3
+	// 2-4, 1-4
+	// 3-7, 5-6
+
+	range1Lower, _ := strconv.Atoi(sectionRange1[0])
+	range1Upper, _ := strconv.Atoi(sectionRange1[1])
+	range2Lower, _ := strconv.Atoi(sectionRange2[0])
+	range2Upper, _ := strconv.Atoi(sectionRange2[1])
+
+	if range1Lower <= range2Lower && range1Upper >= range2Lower {
+		return true
+	} else if range2Lower <= range1Lower && range2Upper >= range1Lower {
 		return true
 	}
 
