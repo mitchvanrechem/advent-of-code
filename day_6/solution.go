63979797d0b477f015a4b2d1c3a1a9c0aebfaadd
Mitch Van Rechem <mitch.vanrechem@inthepocket.com>
2023-01-08 23:30:17 +0100

    ✨ Add day 6 part 2 solution

--- day_6/solution.go
+++ day_6/solution.go
@@ -8,29 +8,32 @@ import (
 func main() {
 	inputLines := utils.ReadInputAsStrings("input.txt")
 
-	solution1 := part1(inputLines[0])
+	solution1 := part1and2(inputLines[0], 4)
+	solution2 := part1and2(inputLines[0], 14)
 
 	utils.PrintSolution(&[]string{
 		fmt.Sprintf("part1: %s", solution1),
+		fmt.Sprintf("part2: %s", solution2),
 	})
 }
 
-func part1(inputLine string) string {
-	for i := 0; i+4 <= len(inputLine); i++ {
-		subString := inputLine[i : i+4]
+func part1and2(inputLine string, packetSize int) string {
+	for i := 0; i+packetSize <= len(inputLine); i++ {
+		subString := inputLine[i : i+packetSize]
 		marker := []rune{}
 		unique := true
 
 		for _, subChar := range subString {
 			if utils.Contains(marker, subChar) {
 				unique = false
+				break
 			}
 
 			marker = append(marker, subChar)
 		}
 
 		if unique {
-			return fmt.Sprint(i + 4)
+			return fmt.Sprint(i + packetSize)
 		}
 	}
 
