266a845c03d4f85c8030acde15bf2e5d27e5f556
Mitch Van Rechem <mitch.vanrechem@inthepocket.com>
2023-01-29 23:35:20 +0100

    ✨ Add day 9 part 2 solution

--- day_9/solution.go
+++ day_9/solution.go
@@ -25,60 +25,65 @@ func main() {
 	moves := parseInput(inputLines)
 
 	// Kept in the logs for visualization (write to a file)
-	solution1 := part1(moves)
+	solution1 := part1and2(moves, 2)
+	solution2 := part1and2(moves, 10)
 
 	utils.PrintSolution(&[]string{
 		fmt.Sprintf("part1: %v", solution1),
+		fmt.Sprintf("part2: %v", solution2),
 	})
 }
 
-func part1(moves []Move) int {
-	tailVisitedMap := makeMoves(moves)
+func part1and2(moves []Move, ropeLength int) int {
+	tailVisitedMap := makeMoves(moves, ropeLength)
 
 	return len(tailVisitedMap)
 }
 
-func makeMoves(moves []Move) TailMap {
+func makeMoves(moves []Move, ropeLength int) TailMap {
 	tailVisitedMap := TailMap{"0,0": void{}}
-	headPosition := Coord{x: 0, y: 0}
-	tailPosition := Coord{x: 0, y: 0}
+	rope := make([]Coord, ropeLength)
+
+	for i := 0; i < ropeLength; i++ {
+		rope[i] = Coord{x: 0, y: 0}
+	}
 
 	for _, move := range moves {
 		fmt.Println("move:", move)
 
 		for i := move.steps; i > 0; i-- {
-			headPosition.moveHead(move.direction)
+			rope[0].moveHead(move.direction)
 
-			fmt.Println("head position:", headPosition)
+			fmt.Println("head position:", rope[0])
 
-			if absDiff(tailPosition.x, headPosition.x) > 1 || absDiff(tailPosition.y, headPosition.y) > 1 {
-				tailPosition.moveTail(headPosition, move.direction)
-
-				tailVisited := fmt.Sprintf("%d,%d", tailPosition.x, tailPosition.y)
-				tailVisitedMap[tailVisited] = void{}
+			for i := 1; i < ropeLength; i++ {
+				if absDiff(rope[i-1].x, rope[i].x) > 1 || absDiff(rope[i-1].y, rope[i].y) > 1 {
+					rope[i].moveTail(rope[i-1], move.direction)
+				}
 			}
 
-			fmt.Println("tail position", tailPosition)
+			tailVisited := fmt.Sprintf("%d,%d", rope[ropeLength-1].x, rope[ropeLength-1].y)
+			tailVisitedMap[tailVisited] = void{}
+
+			fmt.Println("tail position", rope[ropeLength-1])
 		}
 	}
 
 	return tailVisitedMap
 }
 
-func (tailPosition *Coord) moveTail(headPosition Coord, direction string) {
-	switch direction {
-	case "U":
-		tailPosition.x = headPosition.x
-		tailPosition.y = tailPosition.y + 1
-	case "D":
-		tailPosition.x = headPosition.x
-		tailPosition.y = tailPosition.y - 1
-	case "R":
-		tailPosition.x = tailPosition.x + 1
-		tailPosition.y = headPosition.y
-	case "L":
-		tailPosition.x = tailPosition.x - 1
-		tailPosition.y = headPosition.y
+func (curr *Coord) moveTail(prev Coord, direction string) {
+	moveOverAxis(&curr.x, prev.x)
+	moveOverAxis(&curr.y, prev.y)
+}
+
+func moveOverAxis(curr *int, prev int) {
+	if absDiff(*curr, prev) >= 1 {
+		if *curr > prev {
+			*curr -= 1
+		} else {
+			*curr += 1
+		}
 	}
 }
 
